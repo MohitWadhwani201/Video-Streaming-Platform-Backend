22558f8cf7ae33b289b4b969f1c48b965c3cc612
MohitWadhwani201 <mohit.wadhwani2.01@gmail.com>
2025-05-06 07:27:50 +0530

    MONGO connected

---
+++ src/db/dbconnect.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+	try {
+		const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+		console.log(`MongoDB Connected: ${connectionInstance}`);
+	} catch (err) {
+		console.error(`Error: ${err.message}`);
+		process.exit(1); // Exit process with failure
+	}
+};
+
+export default connectDB;
