8a384bbde16ba5a4108327fdc00b0c8ed115fc9b
MohitWadhwani201 <mohit.wadhwani2.01@gmail.com>
2025-05-15 13:08:01 +0530

    All setup done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,11 @@ app.use(express.json({ limit: '16mb' }));
 app.use(express.urlencoded({ limit: '16mb', extended: true }));
 app.use(express.static('public'));
 app.use(cookieParser());
-app.use(cors({ origin:true, credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
+
+import userRouter from './routes/user.routes.js';
+
+//router declaration
+app.use("/api/v1/users", userRouter);
 export { app };
 
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,9 +1,9 @@
-const asyncHandler = (requestHandler) => { 
-    (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((err)=>next(err));
-    }    
-}
-export {asyncHandler};
+const asyncHandler = (requestHandler) => {
+	return (req, res, next) => {
+		Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+	};
+};
+export { asyncHandler };
 //try catch approach
 //  const asyncHandler = (fn) => async (req, res, next) => {
 //     try {
@@ -15,4 +15,4 @@ export {asyncHandler};
 //             message: error.message
 //         })
 //     }
-// } 
+// }
